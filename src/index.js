b498c256421718d30b2615ed639d06a37d04150a
Pedro Ignacio Carmona Sarmiento <nachocarmona@gmail.com>
2017-01-14 23:14:36 +0000

    reading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const fs = require('fs');
 
 server.listen(8081, function (){
   console.log('listening port 8081.');
@@ -16,6 +17,7 @@ indices.on('connection', function (socket){
   });
   socket.on('subscribe', function(room) {
     console.log('joining room', room);
+    watch(room);
     socket.join(room);
   });
 
@@ -26,3 +28,14 @@ indices.on('connection', function (socket){
 
   // indices.sockets.in(data.room).emit('message', data);
 });
+
+function watch (room){
+  const logfile = '/home/gritos/www/admin/logs' + room;
+  fs.watchFile(logfile, function (){
+    console.log('modificado fichero', logfile);
+    fs.readFile(logfile, function (err, data){
+      console.log('emitido modificado', err, data);
+      indices.sockets.in(room).emit('updated', data);
+    });
+  });
+}
