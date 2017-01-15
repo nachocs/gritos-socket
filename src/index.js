0e7908ae848cfb2de842553797c4a9cdfb29e92e
Pedro Ignacio Carmona Sarmiento <nachocarmona@gmail.com>
2017-01-15 19:24:55 +0000

    reading

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 import fs from 'fs';
-
+const directorio = '/home/dreamers/datos/indices/';
+// '/Users/nacho/Google Drive/dreamers/dreamers/datos/indices/peliculas/'
 class App{
   constructor(){
     server.listen(8081, () => {
@@ -31,15 +32,15 @@ class App{
 
   watch (room){
     let logfile;
-    // let dirRoom;
+    let indice;
     room = room.replace(/\/$/,'');
     const logRoom = room.replace(/\//ig, '.');
     if (room === 'foroscomun' || (/\//).test(room)){
       logfile = `/home/indices/admin/logs/${logRoom}.num.txt`;
-      // dirRoom = '/home/indices/' + room;
+      indice = room;
     }  else {
       logfile = `/home/gritos/www/admin/logs/${logRoom}.num.txt`;
-      // dirRoom = '/home/gritos/www/' + room;
+      indice = 'gritos/' + room;
     }
     // logfile = 'src/index.html';
     console.log('watching', room, logfile);
@@ -55,13 +56,37 @@ class App{
           //   indices.in(room).emit('updated');
           // });
         } else {
+          data = data.replace(/\n$/,'');
           console.log('emitido room', room);
           console.log('emitido modificado', err, data);
-          this.indices.in(room).emit('updated', {room, data});
+          this.leer_entrada_indice((Number(data)-1), indice, entry =>{
+            this.indices.in(room).emit('updated', {room, entry});
+          });
         }
       });
     });
   }
+
+  leer_entrada_indice(entrada, indice, callback){
+    fs.readFile(directorio + indice + '/' + entrada + '.txt', { encoding: 'utf8' }, (entryErr, entryData) => {
+      if (entryErr){
+        console.log(entryErr);
+      } else {
+        console.log('entryData', entryData);
+        entryData = entryData.replace(/\n$/,'');
+        const entry = {};
+        const array = entryData.split('\n');
+        for (let i = 0, len = array.length; i< len; i++){
+          const values = array[i].split('\|');
+          values[1] = values[1].replace(/~~/ig, '\|');
+          values[1] = values[1].replace(/``/ig, '\n');
+          entry[values[0]] = values[1];
+        }
+        callback(entry);
+      }
+    });
+  }
 }
+
 const socketApp = new App();
 export default socketApp;
