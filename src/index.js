0abe171dac893609ea634dc88845b44c25fa9c98
Pedro Ignacio Carmona Sarmiento <nachocarmona@gmail.com>
2017-01-15 14:07:17 +0000

    better subscriptions

--- src/index.js
+++ src/index.js
@@ -1,61 +1,67 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const fs = require('fs');
+import fs from 'fs';
 
-server.listen(8081, () => {
-  console.log('listening port 8081.');
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-const indices = io.of('/indices');
-indices.on('connection', socket => {
-  socket.on('disconnect', () => {
-  });
-  socket.on('subscribe', room => {
-    console.log('joining room', room);
-    socket.join(room);
-    watch(room);
-  });
-
-  socket.on('unsubscribe', room => {
-    console.log('leaving room', room);
-    socket.leave(room);
-  });
-
-  // indices.sockets.in(data.room).emit('message', data);
-});
-
-function watch (room){
-  let logfile;
-  // let dirRoom;
-  room = room.replace(/\/$/,'');
-  const logRoom = room.replace(/\//ig, '.');
-  if (room === 'foroscomun' || (/\//).test(room)){
-    logfile = '/home/indices/admin/logs/' + logRoom + '.num.txt';
-    // dirRoom = '/home/indices/' + room;
-  }  else {
-    logfile = '/home/gritos/www/admin/logs/' + logRoom + '.num.txt';
-    // dirRoom = '/home/gritos/www/' + room;
-  }
-  console.log('watching', room, logfile);
-  fs.watchFile(logfile, () => {
-    console.log('modificado fichero', logfile);
-    fs.readFile(logfile, { encoding: 'utf8' }, (err, data) => {
-      if (err){
-        // console.log('error', err);
-        // fs.watch(dirRoom, function (){
-        //   console.log('emitido modificado DIR');
-        //   indices.in(room).emit('updated');
-        // });
-      } else {
-        console.log('emitido room', room);
-        console.log('emitido modificado', err, data);
-        indices.in(room).emit('updated', data);
-      }
+class App{
+  constructor(){
+    server.listen(8081, () => {
+      console.log('listening port 8081.');
     });
-  });
+    app.get('/', (req, res) => {
+      res.sendFile(`${__dirname}/index.html`);
+    });
+    this.watching = {};
+    this.indices = io.of('/indices');
+    this.indices.on('connection', socket => {
+      socket.on('disconnect', () => {
+      });
+      socket.on('subscribe', room => {
+        console.log('joining room', room);
+        socket.join(room);
+        this.watch(room);
+      });
+
+      socket.on('unsubscribe', room => {
+        console.log('leaving room', room);
+        socket.leave(room);
+      });
+    });
+  }
+
+  watch (room){
+    let logfile;
+    // let dirRoom;
+    room = room.replace(/\/$/,'');
+    const logRoom = room.replace(/\//ig, '.');
+    if (room === 'foroscomun' || (/\//).test(room)){
+      logfile = `/home/indices/admin/logs/${logRoom}.num.txt`;
+      // dirRoom = '/home/indices/' + room;
+    }  else {
+      logfile = `/home/gritos/www/admin/logs/${logRoom}.num.txt`;
+      // dirRoom = '/home/gritos/www/' + room;
+    }
+    // logfile = 'src/index.html';
+    console.log('watching', room, logfile);
+    if (this.watching[logfile]){return;}
+    this.watching[logfile] = true;
+    fs.watchFile(logfile, () => {
+      console.log('modificado fichero', logfile);
+      fs.readFile(logfile, { encoding: 'utf8' }, (err, data) => {
+        if (err){
+          console.log('error', err);
+          // fs.watch(dirRoom, function (){
+          //   console.log('emitido modificado DIR');
+          //   indices.in(room).emit('updated');
+          // });
+        } else {
+          console.log('emitido room', room);
+          console.log('emitido modificado', err, data);
+          this.indices.in(room).emit('updated', {room, data});
+        }
+      });
+    });
+  }
 }
+const socketApp = new App();
+export default socketApp;
